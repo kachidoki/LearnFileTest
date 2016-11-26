5b48e23c8f3fed6f177a10fa5e0f8b883ae10796
mayiwei <mayiwei889@126.com>
2016-11-26 23:03:06 +0800

    good

--- app/src/main/java/com/kachidoki/learnfiletest/MainActivity.java
+++ app/src/main/java/com/kachidoki/learnfiletest/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button dlbtn;
     private Button wtbtn;
+    private Button wtPrabtn;
     private ProgressBar progressBar;
     private final int DOWNLOAD_PROGRESS = 1;
     private Handler downloadHandler = new Handler(){
@@ -68,7 +69,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         dlbtn = (Button) findViewById(R.id.downloadBtn);
         wtbtn = (Button) findViewById(R.id.WriteBtn);
+        wtPrabtn = (Button) findViewById(R.id.WritePrivateBtn);
         progressBar = (ProgressBar) findViewById(R.id.progress);
+
+
+        wtPrabtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createPrivateFile();
+            }
+        });
+
 
         wtbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,6 +87,8 @@ public class MainActivity extends AppCompatActivity {
                 createExternalStoragePrivateFile();
             }
         });
+
+
 
         dlbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -97,9 +110,9 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onNext(ResponseBody responseBody) {
                                 if(writeResponseBodyToDisk(responseBody)){
-                                    Log.e("FileDownload","the music is down");
+                                    Log.e("FileDownload","the file is down");
                                 }else {
-                                    Log.e("FileDownload","the music down is fail!!!!!");
+                                    Log.e("FileDownload","the filesdown is fail!!!!!");
                                 }
 
                             }
@@ -115,57 +128,86 @@ public class MainActivity extends AppCompatActivity {
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public void LogFileWhere(){
-        Log.e("File","getDir : "+getDir("getDir",MODE_PRIVATE).getAbsolutePath());
-        Log.e("File","getFilesDir : "+getFilesDir().getAbsolutePath());
-        //        Log.e("File","getExternalMediaDirs : "+getFileStreamPath());
-        Log.e("File","getCacheDir : "+getCacheDir());
-        Log.e("File" ,"getCodeCacheDir : "+getCodeCacheDir());
-//        Log.e("File","getDataDir :"+getDataDir());
-        Log.e("File","getEnternalFilesDir : "+ getExternalFilesDir(Environment.DIRECTORY_MUSIC));
-        Log.e("File","getExternalCacheDir : "+getExternalCacheDir());
+        Log.e("File","内部缓存自定义目录 : "+getDir("getDir",MODE_PRIVATE));
+        Log.e("File","内部储存的根目录 : "+getFilesDir());
+        Log.e("File","内部储存缓存目录 : "+getCacheDir());
+        Log.e("File" ,"内部储存缓存代码目录 : "+getCodeCacheDir());
+
+        Log.e("File","外部储存私有部分根目录 : "+ getExternalFilesDir(null));
+        Log.e("File","外部储存私有部分音乐 : "+ getExternalFilesDir(Environment.DIRECTORY_MUSIC));
+        Log.e("File","外部储存私有部分缓存 : "+getExternalCacheDir());
         for (int i = 0;i<getExternalMediaDirs().length-1;i++){
             Log.e("File","getExternalMediaDirs : "+getExternalMediaDirs()[i].getAbsolutePath());
         }
 
         Log.e("File","-------------------------------------------------------");
-        Log.e("File","getDataDirectory : "+Environment.getDataDirectory().getAbsolutePath());
-        Log.e("File","getDataDirectory : "+Environment.getDataDirectory());
-        Log.e("File","getDownLoadCacheDirectory :"+Environment.getDownloadCacheDirectory().getAbsolutePath());
-        Log.e("File","getExternalStorageDirectory :"+Environment.getExternalStorageDirectory().getAbsolutePath());
+        Log.e("File","Data目录 : "+Environment.getDataDirectory());
+        Log.e("File","外部储存的根目录一般不用:"+Environment.getExternalStorageDirectory());
+        Log.e("File","外部储存的公共文件根目录: "+Environment.getExternalStoragePublicDirectory("Pub_Test"));
+        Log.e("File","下载缓存目录 :"+Environment.getDownloadCacheDirectory());
+        Log.e("File","外部储存的状态: "+Environment.getExternalStorageState());
 
-        Log.e("File","getExternalStoragePublicDirectory : "+Environment.getExternalStoragePublicDirectory("Pub_Test").getAbsolutePath());
-        Log.e("File","getExternalStorageState : "+Environment.getExternalStorageState());
         Log.e("File","DIRECTORY_PICTURES : "+Environment.DIRECTORY_PICTURES);
         Log.e("File","DIRECTORY_MOVIES : "+Environment.DIRECTORY_MOVIES);
     }
 
+    /**
+        写在外部储存应用专属位置demo
+
+     */
     public void createExternalStoragePrivateFile(){
-        String filename = "myfile";
-        String string = "Hello world!";
-        File file = new File(getExternalFilesDir(null),filename);
-        FileOutputStream outputStream;
-        Log.e("Test"," "+file.mkdir());
+
+        File file = new File(getExternalFilesDir(null),"demoFile.jpg");
         try {
-            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
-            outputStream.write(string.getBytes());
-            outputStream.close();
+            InputStream is = getResources().openRawResource(R.drawable.supreme);
+            OutputStream os = new FileOutputStream(file);
+            byte[] data = new byte[is.available()];
+            is.read(data);
+            os.write(data);
+            os.close();
+            is.close();
+            Toast.makeText(MainActivity.this,"图片位置在"+getExternalFilesDir(null),Toast.LENGTH_LONG).show();
         } catch (Exception e) {
             e.printStackTrace();
+            Toast.makeText(MainActivity.this,"出错误了哦",Toast.LENGTH_SHORT).show();
         }
 
     }
     public void deleteExternalStoragePrivateFile() {
         // Get path for the file on external storage.  If external
         // storage is not currently mounted this will fail.
-        File file = new File(getExternalFilesDir(null), "test.jpg");
+        File file = new File(getExternalFilesDir(null), "demoFile.jpg");
         if (file != null) {
             file.delete();
+        }
+    }
+
+    /**
+     *  写在内部储存Demo
+     */
+    private void createPrivateFile(){
+        String filename = "myfile.txt";
+        String string = "Hello world!";
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+            Toast.makeText(MainActivity.this,"文件位置在"+getFilesDir().getAbsolutePath(),Toast.LENGTH_LONG).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this,"出错误了哦",Toast.LENGTH_SHORT).show();
         }
     }
 
 
 
 
+    /**
+     *  使用Retrofit下载文件demo
+     */
     private boolean writeResponseBodyToDisk(ResponseBody body) {
         Log.e("FileDownload","writeResponseBodyToDisk is start");
         try {
